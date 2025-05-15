bdf17f75c1429972d5b98c61b4f7da2c226c9a17
ProggerX <x@proggers.ru>
2025-05-15 10:13:40 +0300

    Remaining API

--- src/Lyrihs/API.hs
+++ src/Lyrihs/API.hs
@@ -7,23 +7,34 @@ module Lyrihs.API (
   getCachedLyrics,
   getLyricsById,
   searchLyrics,
+  requestChallenge,
+  Challenge (..),
   GetResponse (..),
   TrackData (..),
   SearchQuery (..),
+  publish,
+  PublishResponse (..),
+  PublishRequest (..),
 ) where
 
 import Control.Lens ((&), (.~), (^.))
 import Data.Aeson qualified as A
-import Data.ByteString (toStrict)
 import Data.ByteString.Lazy (ByteString)
 import Data.Maybe (fromMaybe)
 import Data.Text (Text)
 import Data.Text qualified as T
-import Data.Text.Encoding (decodeUtf8)
+import Data.Text.Encoding (encodeUtf8)
 import GHC.Generics (Generic)
 import Network.Wreq
 import Prelude hiding (id)
 import Prelude qualified (id)
+
+data Challenge
+  = Challenge
+  { prefix :: Text
+  , target :: Text
+  }
+  deriving (Generic, A.FromJSON)
 
 data TrackData
   = TrackData
@@ -37,6 +48,19 @@ data TrackData
   , syncedLyrics :: Maybe Text
   }
   deriving (Generic, A.FromJSON)
+
+data PublishRequest
+  = PublishRequest
+  { pName :: Text
+  , pArtist :: Text
+  , pAlbum :: Text
+  , pDuration :: Integer
+  , pLyrics :: Maybe Text
+  , pSyncedLyrics :: Maybe Text
+  }
+  deriving (Generic, A.ToJSON)
+
+data PublishResponse = PublishOK | IncorrectToken
 
 instance Show TrackData where
   show TrackData{id, trackName, artistName, albumName} =
@@ -67,18 +91,17 @@ data SearchQuery
 
 type SearchResponse = [TrackData]
 
+decode :: (A.FromJSON a) => ByteString -> a
+decode =
+  fromMaybe (error "Incorrect reply from server")
+    . A.decode
+
 getUrl :: String -> Text -> Text -> Text -> Integer -> IO GetResponse
 getUrl url track artist album duration = do
   resp <- getWith opts url
   case resp ^. responseStatus . statusCode of
     404 -> pure NotFound
-    200 ->
-      pure $
-        OK $
-          fromMaybe (error "Incorrect reply from server on get endpoint") $
-            A.decodeStrictText $
-              decodeUtf8 $
-                toStrict (resp ^. responseBody)
+    200 -> pure $ OK $ decode (resp ^. responseBody)
     _ -> error "Unexpected status code on get endpoint"
  where
   opts =
@@ -91,13 +114,6 @@ getUrl url track artist album duration = do
 getLyrics, getCachedLyrics :: Text -> Text -> Text -> Integer -> IO GetResponse
 getLyrics = getUrl "http://lrclib.net/api/get"
 getCachedLyrics = getUrl "http://lrclib.net/api/get-cached"
-
-decode :: (A.FromJSON a) => ByteString -> a
-decode =
-  fromMaybe (error "Incorrect reply from server")
-    . A.decodeStrictText
-    . decodeUtf8
-    . toStrict
 
 getLyricsById :: Integer -> IO GetResponse
 getLyricsById id' = do
@@ -123,3 +139,19 @@ searchLyrics q = do
         (param "track_name" .~ [qName])
           . maybeParam "artist_name" qArtist
           . maybeParam "album_name" qAlbum
+
+requestChallenge :: IO Challenge
+requestChallenge = do
+  resp <- post "http://lrclib.net/api/search" $ A.toJSON ()
+  pure $ decode (resp ^. responseBody)
+
+publish :: Text -> PublishRequest -> IO PublishResponse
+publish token request = do
+  let body = A.encode request
+  res <- postWith opts "http://lrclib.net/api/publish" body
+  case res ^. responseStatus . statusCode of
+    400 -> pure IncorrectToken
+    201 -> pure PublishOK
+    _ -> error "Unexpected status code on publish endpoint"
+ where
+  opts = defaults & header "X-Publish-Token" .~ [encodeUtf8 token]
